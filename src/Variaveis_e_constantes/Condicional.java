8e8102506ff853120426896f0991687ea6ba4041
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-10 16:37:09 -0300

    Update Condicional.java

--- src/Variaveis_e_constantes/Condicional.java
+++ src/Variaveis_e_constantes/Condicional.java
@@ -3,18 +3,19 @@ package Variaveis_e_constantes;
 import java.util.Scanner;
 
 public class Condicional {
+	static final Integer NOTA_PARA_PASSAR = 70;
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Informe a sua nota: ");
 		int notaDoAluno = scanner.nextInt();
-	
-		if(notaDoAluno>=60) {
-			
+
+		boolean notaParaPassarDeAno = notaDoAluno >= NOTA_PARA_PASSAR;
+
+		if (notaParaPassarDeAno) {
 			System.out.print("Parabéns, você passou de ano.");
-		}
-		else{
+		} else {
 			System.out.print("Parabéns, você reprovou.");
 		}
 	}
