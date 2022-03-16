4c2b2e188c5cb70aef7bf49e70d5325972c6dbb4
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-16 17:23:44 -0300

    commit

--- src/EstruturasCondicionais/Exercicio1.java
+++ src/EstruturasCondicionais/Exercicio1.java
@@ -1,10 +1,30 @@
 package EstruturasCondicionais;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
+	static final float NOTA_MINIMA_PARA_PASSAR = 150;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.print("Informe sua nota na prova de matemática: ");
+		float notaDaProvaDeMatématica = scanner.nextFloat();
+
+		System.out.print("Informe sua nota na prova de português: ");
+		float notaDaProvaDePortugues = scanner.nextFloat();
+
+		float somaDasDuasProvas = notaDaProvaDeMatématica + notaDaProvaDePortugues;
+
+		boolean alcançouANotaMinima = somaDasDuasProvas >= 150;
+		boolean nãoAlcançouAMédiaNaProvaDeMatématica = notaDaProvaDeMatématica < 60;
+		boolean nãoAlcançouAMédiaNaProvaDePortugues = notaDaProvaDePortugues < 60;
+
+		if (alcançouANotaMinima && nãoAlcançouAMédiaNaProvaDeMatématica || nãoAlcançouAMédiaNaProvaDePortugues) {
+			System.out.print("NOTA NA PROVA DE MATEMATICA: " + notaDaProvaDeMatématica
+					+ "\nNOTA NA PROVA DE PORTUGUES: " + notaDaProvaDePortugues + "NOTA FINAL: "+ somaDasDuasProvas);
+
+		}
 	}
 
 }
---
+++ src/EstruturasCondicionais/estruturaIf.java
@@ -0,0 +1,18 @@
+package EstruturasCondicionais;
+
+import java.util.Scanner;
+
+public class estruturaIf {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Informe sua nota na prova de matemática: ");
+		float notaDaProvaDeMatématica = scanner.nextFloat();
+
+		System.out.print("Informe sua nota na prova de português: ");
+		float notaDaProvaDePortugues = scanner.nextFloat();
+
+	}
+
+}
