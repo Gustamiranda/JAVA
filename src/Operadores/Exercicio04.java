5b792b8633701c5f5a93b09e6bb339154843b0b4
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-16 15:09:48 -0300

    commit

--- src/Operadores/Exercicio04.java
+++ src/Operadores/Exercicio04.java
@@ -10,12 +10,12 @@ public class Exercicio04 {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Por favor informe a sua idade: ");
-		byte idade = scanner.nextByte();
+		byte idadeDoUsuario = scanner.nextByte();
 
 		System.out.print("Informe o tempo de contribuição com a previdência: ");
 		byte tempoDeContribuição = scanner.nextByte();
 
-		boolean atenderAosRequisitos = idade >= IDADE_MINIMA_PARA_APOSENTAR
+		boolean atenderAosRequisitos = idadeDoUsuario >= IDADE_MINIMA_PARA_APOSENTAR
 				&& tempoDeContribuição >= TEMPO_MINIMO_DE_CONTRIBUIÇÃO;
 
 		if (atenderAosRequisitos) {
