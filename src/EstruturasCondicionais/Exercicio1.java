03f488f44e917cc7048bc11d9725f861bd36dfe9
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-17 14:31:05 -0300

    commit

--- src/EstruturasCondicionais/Exercicio1.java
+++ src/EstruturasCondicionais/Exercicio1.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Exercicio1 {
 	static final float NOTA_MINIMA_PARA_PASSAR = 150;
 
+	public static void mostrarNotas() {
+
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
@@ -14,17 +18,36 @@ public class Exercicio1 {
 		System.out.print("Informe sua nota na prova de português: ");
 		float notaDaProvaDePortugues = scanner.nextFloat();
 
-		float somaDasDuasProvas = notaDaProvaDeMatématica + notaDaProvaDePortugues;
+		float somaDasDuasProvas = somaDasNotas(notaDaProvaDeMatématica, notaDaProvaDePortugues);
 
 		boolean alcançouANotaMinima = somaDasDuasProvas >= 150;
 		boolean nãoAlcançouAMédiaNaProvaDeMatématica = notaDaProvaDeMatématica < 60;
 		boolean nãoAlcançouAMédiaNaProvaDePortugues = notaDaProvaDePortugues < 60;
 
-		if (alcançouANotaMinima && nãoAlcançouAMédiaNaProvaDeMatématica || nãoAlcançouAMédiaNaProvaDePortugues) {
-			System.out.print("NOTA NA PROVA DE MATEMATICA: " + notaDaProvaDeMatématica
-					+ "\nNOTA NA PROVA DE PORTUGUES: " + notaDaProvaDePortugues + "NOTA FINAL: "+ somaDasDuasProvas);
+		if (nãoAlcançouAMédiaNaProvaDeMatématica || nãoAlcançouAMédiaNaProvaDePortugues && alcançouANotaMinima) {
 
+			System.out.print("Nota na prova de matématica: " + notaDaProvaDeMatématica
+					+ "\nNota na prova de português: " + notaDaProvaDePortugues + "\nNota final: " + somaDasDuasProvas);
+
+			System.out.print("\nInfelizmente você não passou.");
+		} else if (alcançouANotaMinima) {
+			System.out.print("Nota na prova de matématica: " + notaDaProvaDeMatématica
+					+ "\nNota na prova de português: " + notaDaProvaDePortugues + "\nNota final: " + somaDasDuasProvas);
+
+			System.out.print("\nParabéns, você passou.");
+		} else {
+
+			System.out.print("Nota na prova de matématica: " + notaDaProvaDeMatématica
+					+ "\nNota na prova de português: " + notaDaProvaDePortugues + "\nNota final: " + somaDasDuasProvas);
+
+			System.out.print("\nInfelizmente você não passou.");
 		}
+
+	}
+
+	public static float somaDasNotas(float notaDaProvaDeMatématica, float notaDaProvaDePortugues) {
+
+		return notaDaProvaDeMatématica + notaDaProvaDePortugues;
 	}
 
 }
--- src/EstruturasCondicionais/estruturaIf.java
+++ src/EstruturasCondicionais/estruturaIf.java
@@ -5,14 +5,8 @@ import java.util.Scanner;
 public class estruturaIf {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Informe sua nota na prova de matemática: ");
-		float notaDaProvaDeMatématica = scanner.nextFloat();
-
-		System.out.print("Informe sua nota na prova de português: ");
-		float notaDaProvaDePortugues = scanner.nextFloat();
-
+		
+		
 	}
 
 }
