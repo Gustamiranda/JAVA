efc9a5ff15ab7f290425a8b8551cac6416aded54
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-29 15:11:59 -0300

    commit

--- src/estruturaDeDecisao/Exercicio02.java
+++ src/estruturaDeDecisao/Exercicio02.java
@@ -6,13 +6,18 @@ public class Exercicio02 {
 
 	public static void main(String[] args) {
 
-		double metaDeFaturamento = receberMetaDeFaturamentoAnual();
+		double metaDeFaturamentoAnual = receberMetaDeFaturamentoAnual();
 		double faturamentoRealDaEmpresaNoUltimoAno = receberFaturamentoRealDaEmpresaNoUltimoAno();
 		double mediaDoSalarioDoFuncionario = receberMediaSalarialDoFuncionarioParaOAnoAnterior();
+		double OitentaPorcentoDaMeta = calculoDaPorcentagem(metaDeFaturamentoAnual);
 
-		boolean funcionarioPodeReceberOBonus = darBonusAoFuncionario(faturamentoRealDaEmpresaNoUltimoAno,
-				metaDeFaturamento);
+		boolean FaturamentoFoiMaiorOuIgualMeta = verificarSeFaturamentoFoiMaiorOuIgualMeta(
+				faturamentoRealDaEmpresaNoUltimoAno, metaDeFaturamentoAnual);
+		boolean FaturamentoFoiIgualOuMaiorAOitentaPorcentoDaMeta = verificarSeFaturamentoFoiIgualOuMaiorAOitentaPorcentoDaMeta(
+				OitentaPorcentoDaMeta, metaDeFaturamentoAnual, faturamentoRealDaEmpresaNoUltimoAno);
 
+		ExibirResultado(FaturamentoFoiMaiorOuIgualMeta, mediaDoSalarioDoFuncionario,
+				FaturamentoFoiIgualOuMaiorAOitentaPorcentoDaMeta, OitentaPorcentoDaMeta);
 	}
 
 	public static double receberMetaDeFaturamentoAnual() {
@@ -31,9 +36,34 @@ public class Exercicio02 {
 				JOptionPane.showInputDialog("Informe a  média salarial do funcionário(a) para o ano anterior"));
 	}
 
-	public static boolean darBonusAoFuncionario(double faturamentoRealDaEmpresaNoUltimoAno, double metaDeFaturamento) {
+	public static boolean verificarSeFaturamentoFoiMaiorOuIgualMeta(double faturamentoRealDaEmpresaNoUltimoAno,
+			double metaDeFaturamentoAnual) {
 
-		return faturamentoRealDaEmpresaNoUltimoAno >= metaDeFaturamento;
+		return faturamentoRealDaEmpresaNoUltimoAno >= metaDeFaturamentoAnual;
 	}
 
+	public static double calculoDaPorcentagem(double metaDeFaturamentoAnual) {
+
+		return (0.80 * metaDeFaturamentoAnual) / 100;
+	}
+
+	public static boolean verificarSeFaturamentoFoiIgualOuMaiorAOitentaPorcentoDaMeta(double PorcentagemDe80DaMeta,
+			double metaDeFaturamentoAnual, double faturamentoRealDaEmpresaNoUltimoAno) {
+
+		return faturamentoRealDaEmpresaNoUltimoAno < metaDeFaturamentoAnual
+				&& faturamentoRealDaEmpresaNoUltimoAno >= PorcentagemDe80DaMeta;
+	}
+
+	public static void ExibirResultado(boolean FaturamentoFoiMaiorOuIgualMeta, double mediaDoSalarioDoFuncionario,
+			boolean FaturamentoFoiIgualOuMaiorAOitentaPorcentoDaMeta, double OitentaPorcentoDaMeta) {
+
+		if (FaturamentoFoiMaiorOuIgualMeta) {
+
+			JOptionPane.showMessageDialog(null, "Seu prêmio será: " + mediaDoSalarioDoFuncionario);
+		} else if (FaturamentoFoiIgualOuMaiorAOitentaPorcentoDaMeta) {
+
+			JOptionPane.showMessageDialog(null, "Seu prêmio será: " + OitentaPorcentoDaMeta);
+		}
+
+	}
 }
