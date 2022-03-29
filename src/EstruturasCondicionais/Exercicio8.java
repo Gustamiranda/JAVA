6411193b4897ce76d80f391c11b1671352e75594
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-29 15:23:29 -0300

    commit

--- src/EstruturasCondicionais/Exercicio8.java
+++ src/EstruturasCondicionais/Exercicio8.java
@@ -1,9 +1,17 @@
 package EstruturasCondicionais;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio8 {
 
 	public static void main(String[] args) {
-		
+
+		double[] precos = new double[3];
+
+		for (int c = 0; c < precos.length; c++) {
+
+			JOptionPane.showMessageDialog(null, "Informe o preço do " + (c+1) + "° produto: ");
+		}
 
 	}
 
