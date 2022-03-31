cc2c44ca891b740a67a38e7da88b3c718f909cc6
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-03-31 14:42:19 -0300

    commit

--- src/EstruturasCondicionais/Exercicio9.java
+++ src/EstruturasCondicionais/Exercicio9.java
@@ -5,26 +5,43 @@ import java.util.Scanner;
 public class Exercicio9 {
 
 	static final Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
 
-		 receberTresNumeros();
+		receberEOrdenarTresNumeros();
 	}
 
-	public static void receberTresNumeros() {
-		
+	public static void receberEOrdenarTresNumeros() {
+
 		int[] numeros = new int[3];
-		
-		for(int c = 0; c< numeros.length; c++) {
-			
-			System.out.print("Digite o "+ (c+1) +"° número: ");
-			numeros[c] = scanner.nextInt();
-		
-		}
-	
-	
-	
-	}
-	
 
+		for (int c = 0; c < numeros.length; c++) {
+
+			System.out.print("Digite o " + (c + 1) + "° número: ");
+			numeros[c] = scanner.nextInt();
+		}
+
+		boolean troca = true;
+		int aux;
+		while (troca) {
+			troca = false;
+			for (int i = 0; i < numeros.length - 1; i++) {
+				if (numeros[i] < numeros[i + 1]) {
+					aux = numeros[i];
+					numeros[i] = numeros[i + 1];
+					numeros[i + 1] = aux;
+					troca = true;
+				}
+			}
+
+		}
+		System.out.print("Numeros em ordem decresente: ");
+		for (int b = 0; b < numeros.length; b++) {
+
+			System.out.print(numeros[b] + " , ");
+
+		}
+
+	}
 
 }
