f05ea0003cbaada3645ef6200fe31525eaaa4cb8
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-04-05 17:22:43 -0300

    commit

--- src/EstruturasCondicionais/Exercicio12.java
+++ src/EstruturasCondicionais/Exercicio12.java
@@ -1,9 +1,38 @@
 package EstruturasCondicionais;
 
+import java.util.Scanner;
+
 public class Exercicio12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Informe  o valor da sua hora: ");
+		double valorDaHora = scanner.nextInt();
+
+		System.out.print("Informe a quantidade de horas trabalhadas no mês: ");
+		double HorasTrabalhadas = scanner.nextInt();
+
+		double salarioBruto = HorasTrabalhadas * valorDaHora;
+
+		boolean salarioAte900 = salarioBruto > 0 && salarioBruto <= 900;
+		boolean salarioAte1500 = salarioBruto > 900 && salarioBruto <= 1500;
+		boolean salarioAte2500 = salarioBruto > 1500 && salarioBruto <= 2500;
+		boolean salarioAcimaDe2500 = salarioBruto > 2500;
+
+		if (salarioAte900) {
+
+			double descontoDoIR = salarioBruto * 0.5;
+			double descontoDoINSS = salarioBruto * 0.10;
+			double descontoDoFGTS = salarioBruto * 0.11;
+			double totalDeDescontos = descontoDoIR + descontoDoINSS + descontoDoFGTS;
+			
+			System.out.print("Salário bruto: " + salarioBruto + "R$ ");
+			System.out.println("Total de descontos: " + totalDeDescontos + "R$");
+			System.out.println("Salário liquido: " + (salarioBruto - totalDeDescontos)+ "");
+		
+		}
 
 	}
 
