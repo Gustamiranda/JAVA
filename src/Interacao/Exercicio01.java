ee2e5db3ae70d3a8c998b597485c5a57e650f57b
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-04-07 16:22:38 -0300

    commit

---
+++ src/Interacao/Exercicio01.java
@@ -0,0 +1,24 @@
+package Interacao;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	static final Integer DIVISOR = 3;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Integer numeros[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+		for (int i = 0; i < numeros.length; i++) {
+
+			Integer divisao = numeros[i] % DIVISOR;
+
+			if (divisao.equals(0)) {
+				System.out.print("\n O numero " + numeros[i] + " é divisível por 3");
+			}
+		}
+		scanner.close();
+	}
+}
