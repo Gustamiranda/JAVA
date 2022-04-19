6c216f0656e1236f2caadd3d61651e799e77704e
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-04-19 16:25:45 -0300

    commit

--- src/Metodos/Exercicio02.java
+++ src/Metodos/Exercicio02.java
@@ -1,17 +1,23 @@
 package Metodos;
 
+/*
+Identifique um novo padrão no algoritmo do código anterior.
+Estude um pouco mais o algoritmo do código passado, e identifique algum trecho dele que pode ser separado para colocar
+em um novo método criado por você.
+ */
+
 import java.util.Scanner;
 
-public class Exercicio01 {
+public class Exercicio02 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
 		imprimirTraco();
 		String[] cursos = new String[] { "Java EE", "Spring", "Java OO Avançado" };
-		System.out.println("Escolha dentre os cursos abaixo: ");
-		for (int i = 0; i < cursos.length; i++) {
-			System.out.println("[" + i + "] " + cursos[i]);
-		}
+
+		mostrarVetor(cursos);
+
 		System.out.print("O curso que você deseja é o: ");
 		Integer posicaoCursoEscolhido = scanner.nextInt();
 		Boolean posicaoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
@@ -23,15 +29,17 @@ public class Exercicio01 {
 		imprimirTraco();
 		String[] formasPagamento = new String[] { "Cartão", "Boleto" };
 		System.out.println("Escolha dentre as formas de pagamento abaixo: ");
-		for (int i = 0; i < formasPagamento.length; i++) {
-			System.out.println("[" + i + "] " + formasPagamento[i]);
-		}
+
+		mostrarVetor(formasPagamento);
+
 		System.out.print("Sua forma de pagamento escolhida é: ");
 		Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
 		posicaoValida = posicaoFormaPagamentoEscolhida >= 0 && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+
 		if (!posicaoValida) {
 			exibirERRO();
 		}
+
 		String cursoEscolhido = cursos[posicaoCursoEscolhido];
 		String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
 		imprimirTraco();
@@ -47,6 +55,12 @@ public class Exercicio01 {
 	static void exibirERRO() {
 		System.err.println("Posição inválida!");
 		System.exit(1);
+	}
+
+	static void mostrarVetor(String vetor[]) {
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println("[" + i + "] " + vetor[i]);
+		}
 	}
 
 }
