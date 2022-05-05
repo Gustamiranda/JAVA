d684ee7344be230f2ef1bbf31e4d8c2fd46ca8b1
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-05-05 15:53:18 -0300

    commit

---
+++ src/Leitura_e_escrita_de_dados_em_arquivos/EscritaArquivos.java
@@ -0,0 +1,30 @@
+package Leitura_e_escrita_de_dados_em_arquivos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EscritaArquivos {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+
+		ArrayList<String> linhas = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Jogador " + i + ": ");
+			String nome = scanner.nextLine();
+
+			linhas.add(nome);
+		}
+		Path arquivo = Paths.get("C:\\Users\\aluno.LABCETEC\\Desktop.arquivo.txt");
+
+		Files.write(arquivo, linhas);
+
+		scanner.close();
+
+	}
+}
