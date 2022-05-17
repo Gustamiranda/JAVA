4fa397d44b1ba6e27aff45b920996925029ad59e
Gustamiranda <gustavo.miranda@es.estudante.senai.br>
2022-05-17 17:19:51 -0300

    COMMIT

--- src/Orientacao_a_objetos_parte2/Calculadora/TesteCalculadora.java
+++ src/Orientacao_a_objetos_parte2/Calculadora/TesteCalculadora.java
@@ -3,15 +3,14 @@ package Orientacao_a_objetos_parte2.Calculadora;
 public class TesteCalculadora {
 
 	public static void main(String[] args) {
-		
-		 	OperacaoAritmetica o1 = OperacaoAritmetica.ADICAO;
-	        int resultado1 = o1.operacao(5, 3);
-	        System.out.println("O resultado é: " + resultado1);
-	        OperacaoAritmetica o2 = OperacaoAritmetica.SUBTRACAO;
-	        int resultado2 = o2.operacao(5, 3);
-	        System.out.println("O resultado é: " + resultado2);
-		 
-	
+
+		OperacaoAritmetica o1 = OperacaoAritmetica.ADICAO;
+		int resultado1 = o1.operacao(5, 3);
+		System.out.println("O resultado é: " + resultado1);
+		OperacaoAritmetica o2 = OperacaoAritmetica.SUBTRACAO;
+		int resultado2 = o2.operacao(5, 3);
+		System.out.println("O resultado é: " + resultado2);
+
 	}
 
 }
--- src/Orientacao_a_objetos_parte2/desafio_objeto_this_e_construtores/SituacaoConta.java
+++ src/Orientacao_a_objetos_parte2/desafio_objeto_this_e_construtores/SituacaoConta.java
@@ -1,5 +1,13 @@
 package Orientacao_a_objetos_parte2.desafio_objeto_this_e_construtores;
 
 public enum SituacaoConta {
+	PAGA, CANCELADA
+	
+	
+
+	
+
+
+
 
 }
